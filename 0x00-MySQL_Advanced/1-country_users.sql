ec3959ab39e856401f3fc2a70074d044aa71f42c
Ahmed Walid <ahmedwalid@pop-os.localdomain>
2024-01-19 00:43:20 +0200

    sql advanced

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
--- create table users
+-- Creates a table with unique users.
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-    id INT NOT NULL AUTO_INCERMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    country VARCHAR(2) NOT NULL DEFAULT  'US' CHECK (country in ('US', 'CO', 'TN'))
+                       id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                       email VARCHAR(255) NOT NULL UNIQUE,
+                       name VARCHAR(255),
+                       country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
 );
