68e60a2081b05a17363a63290a3d330a4e241c73
Ahmed Walid <ahmedwalid@pop-os.localdomain>
2024-01-24 20:07:03 +0200

    no-sql

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -24,3 +24,22 @@ class Cache:
         key: str = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+            self, key: str, fn: Union[Callable, None] = None
+    ) -> Union[str, bytes, int, float]:
+        """get value and pass it to the callable"""
+        value = self._redis.get(key)
+
+        if fn is not None:
+            return fn(value)
+
+        return value
+
+    def get_str(self, key: str) -> str:
+        """parametrize method for getting a string from the cache"""
+        return self.get(key, lambda x: x.decode("utf-8"))  # type: ignore
+
+    def get_int(self, key: str) -> int:
+        """parametrize method for getting an integer from the cache"""
+        return self.get(key, int)  # type: ignore
